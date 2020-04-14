94d7909ede74706c7fa37c8e1ccf5f3330ac24f1
Avi <avibhusal13@gmail.com>
2020-04-15 00:27:28 +1000

    Created a event listener for Add, delete and checked

---
+++ app.js
@@ -0,0 +1,36 @@
+//Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+//Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+//Functions
+
+function addTodo(e) {
+  //Prevent form from submiting
+  e.preventDefault();
+  //TODO DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //Checked Button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Delete Button
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML = '<i class= "fas fa-trash"></i>';
+  deleteButton.classList.add("delete-btn");
+  todoDiv.appendChild(deleteButton);
+  //Append to List
+  todoList.appendChild(todoDiv);
+  //Clear todo Input Value
+  todoInput.value = "";
+}
